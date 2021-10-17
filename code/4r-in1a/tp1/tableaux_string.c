ea3ea56679ec00facd36b4169fca12064fd76f8d
amineaupec <you@example.com>
2021-10-17 19:50:49 +0200

    tabstring

--- code/4r-in1a/tp1/tableaux_string.c
+++ code/4r-in1a/tp1/tableaux_string.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <string.h>
 
 unsigned char test_string[] = "reinvented around you";
 
@@ -12,42 +11,20 @@ int main(void)
 int print_string(int argc, char *argv[])
 {
 
-
-
-       char *buffer;
-   char command[512];
-   int i = 1;
-   for(i = 1; i < argc; i++){
-     buffer = malloc(strlen(argv[i]) + 5);
-     strcpy(buffer,argv[i]);
-     strcat(buffer,".txt");
-     sprintf(command,"touch %s\0",buffer);
-     system(command);
-     free(buffer);
-  }
     for (int i = 0; i < argc; i++)
     {
 
-        char all_arg[] = "";
-
         if (i == 0)
         {
-            printf("%d \n", argc);
+            printf("DEBUG : Number of arg %d \n", argc);
+            printf("Résultat de la chaîne de caractère : "); // at the last iteration
         }
 
-        printf("argv[%d] = %s ", i, argv[i]);
-        strcat(all_arg, " ");
-        strcat(all_arg, argv[i]);
-
+        printf("%s ", argv[i]);
         if (i == argc - 1)
         {
-            printf("last_iter : %d \n", i);
-
-            printf("allarg: %s \n", all_arg);
-            printf("ok %c", all_arg);
+            printf("\n"); // at the last iteration
         }
-
-        // printf("argv[%d] = %s\n", i, argv[i]);
     }
     // printf("Résultat de la chaîne de caractère : %s \n", mystring);
     return 0;
