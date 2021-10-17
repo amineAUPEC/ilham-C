b7305a7cb347ea34f5c6bd41b4601109d7e3879f
amineaupec <you@example.com>
2021-10-17 19:40:30 +0200

    tabstring

--- code/4r-in1a/tp1/tableaux_string.c
+++ code/4r-in1a/tp1/tableaux_string.c
@@ -1,41 +1,40 @@
 #include <stdio.h>
+#include <string.h>
 
-unsigned char test_string[]="reinvented around you";
+unsigned char test_string[] = "reinvented around you";
 
-int main(void) {
-    int resultat=print_string(test_string);
+int main(void)
+{
+    int resultat = print_string();
     return 0;
-
 }
 
+int print_string(int argc, char *argv[])
+{
+    for (int i = 0; i < argc; i++)
+    {
 
+        char all_arg[] = "";
 
-int print_string(char mystring[]){
-    printf("Résultat de la chaîne de caractère : %s \n", mystring);
-    return 0; 
+        if (i == 0)
+        {
+            printf("%d \n", argc);
+        }
+
+        printf("argv[%d] = %s \n", i, argv[i]);
+        strcat(all_arg, " ");
+        strcat(all_arg, argv[i]);
+
+        if (i == argc - 1)
+        {
+            printf("last_iter : %d \n", i);
+
+            printf("allarg: %s \n", all_arg);
+            // printf("ok %c", all_arg);
+        }
+
+        // printf("argv[%d] = %s\n", i, argv[i]);
+    }
+    // printf("Résultat de la chaîne de caractère : %s \n", mystring);
+    return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
