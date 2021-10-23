1a5bb104e18627ca5e0f302741a8ddd1c16b5d23
amineaupec <amine.abdoul-azid@etu.u-pec.fr>
2021-10-23 21:43:08 +0200

    2143

--- code/4r-in1a/tp1/struct.c
+++ code/4r-in1a/tp1/struct.c
@@ -1,69 +1,52 @@
 // https://www.tutorialspoint.com/cprogramming/c_structures.htm
+// https://www.commentcamarche.net/contents/119-les-structures-en-langage-c
 #include <stdio.h>
 #include <string.h>
 
 
-// struct t_Date{
-//     unsigned int jour, mois, an; 
-// };
+struct t_Date{
+    unsigned int jour, mois, an; 
+};
 
 
-// struct t_Identite
-// {
-//     #define nom[10]="";
-//     #define prenom[10]="";
-//     char personne[20];
-//     personne=strcat(personne, nom);
-//     personne=strcat(personne, prenom);
-// };
+struct t_Identite
+{
+    #define nom [10]="";
+    #define prenom [10]="";
+    char personne[20];
+    // personne=strcat(personne, nom);
+    // personne=strcat(personne, prenom);
+};
 
 
-// struct t_Adresse
-// {
-//     int zip_code;
-//     char ville;
+struct t_Adresse
+{
+    int zip_code;
+    char ville;
     
-// };
-
-
-int annee_test=2016;
-if (annee_test % 400 == 0){
-    printf("Cette année est bissextile \n");
-    return 1;
-}
-else if (annee_test % 4 == 0 && annee_test %100 != 0){
-    printf("Cette année est bissextile \n");
-    return 11;
-}
-else {
-    printf("Cette année n'est pas bissextile \n");
-    return 0;
-}
-
-#include <stdio.h>
-
+};
 
 int main(void)
 {
-    int resultat = annee_test();
+    int resultat = est_bissextile(2015);
     return 0;
 }
 
-int annee_test()
+int est_bissextile(int annee_test)
 {
+    printf("L'année : %d \n", annee_test);
 
-  int annee_test=1996;
-  if (annee_test % 400 == 0){
-      printf("Cette année est bissextile nope");
-      return 1;
-  }
-  else if (annee_test % 4 == 0 && annee_test %100 != 0){
-      printf("Cette année est bissextile ok");
-      return 11;
-  }
-  else {
-      printf("Cette année n'est pas bissextile");
-      return 0;
-  }
+    if (annee_test % 400 == 0){
+        printf("Cette année est bissextile ! \n");
+        return 1;
+    }
+    else if (annee_test % 4 == 0 && annee_test %100 != 0){
+        printf("Cette année est bissextile !! \n");
+        return 1;
+    }
+    else {
+        printf("Cette année n'est pas bissextile \n");
+        return 0;
+    }
     return 0;
 }
